3a475fba9dbc50b537d7f33bd2789808f6031c7a
Pedro Seda <pedroseda@gmail.com>
2018-11-29 20:20:18 -0200

    Move configuration class inside module

--- lib/nfe/configuration.rb
+++ lib/nfe/configuration.rb
@@ -1,8 +1,10 @@
-class Configuration
-  attr_accessor :url, :user_agent
+module Nfe
+  class Configuration
+    attr_accessor :url, :user_agent
 
-  def initialize
-    @url = "https://api.nfe.io"
-    @user_agent = "NFe.io Ruby Client v#{Nfe::VERSION}"
+    def initialize
+      @url = "https://api.nfe.io"
+      @user_agent = "NFe.io Ruby Client v#{Nfe::VERSION}"
+    end
   end
 end
